5d65cf4247f3bf97d4e2d866989db6d892791251
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-24 18:49:23 +0600

    parts done

--- index.js
+++ index.js
@@ -10,8 +10,23 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kwnxt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
+  await client.connect();
+  const partsCollection = client.db("Manufacturing").collection("parts");
   try {
+    // find all parts
+    app.get("/parts", async (req, res) => {
+      const query = {};
+      const parts = await partsCollection.find(query).toArray();
+      res.send(parts);
+    });
   } finally {
   }
 }
