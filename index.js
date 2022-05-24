a477d3dec4fc142ad9a820e8e40d3362f01a333c
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-24 17:57:52 +0600

    create first route for deployment testing

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors App listening on port ${port}`);
+});
